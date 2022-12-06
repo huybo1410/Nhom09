7659cbe0c2a13cb790b73c737c833738dab106be
Nguyen Quang Ninh <0306201267@caothang.edu.vn>
2022-12-06 13:56:54 +0700

    up

---
+++ Nhom09/Areas/Admin/Controllers/productsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nhom09.Areas.Admin.Controllers
+{
+    public class productsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nhom09/Areas/Admin/Models/product.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Nhom09.Areas.Admin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+        [DisplayName("Tên sản phẩm")]
+        [Required(ErrorMessage = "{0} không được bỏ trống")]
+        public string Name { get; set; }
+
+        [DisplayName("Giá (VNĐ)")]
+        [DisplayFormat(DataFormatString = "{0:n0} đ")]
+        public int Price { get; set; }
+
+
+        [DisplayName("Số Lượng")]
+        public int Quantity { get; set; }
+
+
+        [DisplayName("Ảnh Minh Họa")]
+        public string Image { get; set; }
+
+        [DisplayName("Loại sản phẩm")]
+        public int ProductTypeId { get; set; }
+
+        // Reference navigation property cho khóa ngoại đến ProductType
+        [DisplayName("Loại sản phẩm")]
+        public ProductType ProductType { get; set; }
+
+        public List<InvoiceDetail> Invoice_Details { get; set; }
+
+
+
+        public string Chip { get; set; }
+        public string RAM { get; set; }
+        public string ScreenSize { get; set; }
+
+        public string Pin { get; set; }
+
+    }
+}
