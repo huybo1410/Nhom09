e57914405e6ecbd344bc251a66d98035aba02d68
Nguyen Quang Ninh <0306201267@caothang.edu.vn>
2022-12-27 14:49:14 +0700

    update search

--- Nhom09/Controllers/HomeController.cs
+++ Nhom09/Controllers/HomeController.cs
@@ -25,10 +25,50 @@ namespace Nhom09.Controllers
             var lsProductType = _context.ProductTypes.ToList();
             ViewBag.lsProductType = lsProductType;
             return View(_context.Products.ToList());
-            //ViewBag.username = HttpContext.Session.GetString("username");
-            //return View();
         }
+        public IActionResult AllProduct()
+        {
+            var lsProductType = _context.ProductTypes.ToList();
+            ViewBag.lsProductType = lsProductType;
+            return View(_context.Products.ToList());
+        }
+        public IActionResult Search(int? id)
+        {
+            var lsProductType = _context.ProductTypes.ToList();
+            ViewBag.lsProductType = lsProductType;
 
+            if (id == null)
+            {
+                return View(_context.Products.ToList());
+               
+            }
+            var lstProduct = _context.Products.Where(x => x.ProductTypeId == id).ToList();
+            lsProductType = _context.ProductTypes.ToList();
+            ViewBag.lsProductType = lsProductType;
+            return View(lstProduct);
+           
+
+
+        }
+        public async Task<IActionResult> Searchs(string searchString)
+        {
+
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            var product = from m in _context.Products
+                          select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                product = product.Where(s => s.Name!.Contains(searchString));
+            }
+            var lsProductType = _context.ProductTypes.ToList();
+            ViewBag.lsProductType = lsProductType;
+            return View(await product.ToListAsync());
+        }
         public IActionResult Privacy()
         {
             return View();
